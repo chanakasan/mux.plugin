45a5f24e7e386d4d6b50adebca5cae57ea9a4db8
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-02-12 12:18:47 +0530

    rev

--- bash/load_underscore_mux.bash
+++ bash/load_underscore_mux.bash
@@ -1,11 +1,10 @@
 source $nex_mux_path/lib/underscore_mux.sh
 
-alias learn="nx__learn"
-alias build="nx__build"
-alias try="nx__try"
-alias play="nx__play"
-alias repo="nx__repo"
-alias work="nx__work"
-
-alias dev="mux dev"
-alias proj="mux proj"
+alias learn="nx_mux learn"
+alias build="nx_mux build"
+alias try="nx_mux try"
+alias work="nx_mux work"
+alias dev="nx_mux dev"
+alias proj="nx_mux proj"
+alias repo="nx_mux repo $HOME/repos"
+alias play="nx_mux play $HOME/playDoh"
--- lib/underscore_mux.sh
+++ lib/underscore_mux.sh
@@ -1,30 +1,9 @@
-nx__try() {
-  nx_mux_cd try "$HOME/@sb/try"
-}
-
-nx__build() {
-  nx_mux_cd build "$HOME/@sb/build"
-}
-
-nx__learn() {
-  nx_mux_cd learn "$HOME/@sb/learn"
-}
-
-nx__work() {
-  nx_mux_cd work "$HOME/@sb/work"
-}
-
-nx__repo() {
-  nx_mux_cd repo "$HOME/repos"
-}
-
-nx__play() {
-  nx_mux_cd playdoh "$HOME/playDoh"
-}
-
-nx_mux_cd() {
+nx_mux() {
   local name="$1"
   local base="$2"
+  if [ -z $base ]; then
+    base=$HOME/@sb/$name
+  fi
   local is_tmux=$(tmux-nested)
   local current=$(tmux-current-session)
   if [ "$current" == "$name" ]; then

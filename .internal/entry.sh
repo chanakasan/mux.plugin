4eda78b4be5e506dfca74d3d09fe36ef6cbef6ef
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-05-25 14:45:25 +0530

    refac

--- .internal/entry.sh
+++ .internal/entry.sh
@@ -1,10 +1,6 @@
 export nx_mux_root=$(nex _plugin mux)
 export PATH=$nx_mux_root/bin:$PATH
-
-# mixins
-for f in $nx_mux_root/.internal/mixins/*.sh; do
-  source $f
-done
+source $nx_mux_root/.internal/core/mux-cd.sh
 
 # config
 for f in $nx_mux_root/src/config/*.sh; do

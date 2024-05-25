7c1738bdbe2a665f4e07d83c740164d3112504e8
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-05-25 14:40:57 +0530

    refac

--- .internal/entry.sh
+++ .internal/entry.sh
@@ -1,6 +1,12 @@
-export nex_mux_path=$(nex _plugin mux)
-export PATH=$nex_mux_path/bin:$PATH
+export nx_mux_root=$(nex _plugin mux)
+export PATH=$nx_mux_root/bin:$PATH
 
-for f in $nex_mux_path/src/bash/*.sh; do
+# mixins
+for f in $nx_mux_root/.internal/mixins/*.sh; do
+  source $f
+done
+
+# config
+for f in $nx_mux_root/src/config/*.sh; do
   source $f
 done

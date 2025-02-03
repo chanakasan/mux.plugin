c8781328a0b10198ae3b8352215078e125bcb587
Chanaka Abeysinghe <chanakasan@gmail.com>
2025-02-03 12:07:07 +0530

    refac

--- .internal/entry.sh
+++ .internal/entry.sh
@@ -1,5 +1,5 @@
 export nx_mux_root=$(nex _plugin mux)
 export PATH=$nx_mux_root/bin:$PATH
 
-source $nx_mux_root/.internal/concerns/mux-cd.sh
-source $nx_mux_root/src/includes/alias.sh
+source $nx_mux_root/userland/ext/mux-cd.sh
+source $nx_mux_root/userland/config/alias.sh
--- userland/config/alias.sh
+++ userland/config/alias.sh
@@ -3,9 +3,9 @@ alias desk="nx_mux_cd desk ~/workspace/desk"
 alias practice="nx_mux_cd practice ~/workspace/practice-desk"
 
 #-- spaces
+alias work="nx_mux_cd work ~/workspace"
+alias lab="nx_mux_cd lab ~/lab"
 alias home="nx_mux_cd home ~/home"
-alias work="nx_mux_cd desk ~/workspace"
-alias lab="nx_mux_cd desk ~/workspace"
 alias temp="nx_mux_cd temp ~/temp"
 alias con="nx_mux_cd conn ~/conn"
 alias proc="nx_mux_cd proc ~/proc"
